ad3a1ad7f2b437f88ea6a4b0af85b5af121a31ae
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-05-02 17:08:51 +0200

    feat(cakes): definition of constants

---
+++ cakes/src/main/java/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+/*
+cake baking problem
+Example 2.1.2 of Minizinc https://www.minizinc.org/doc-2.5.5/en/modelling.html
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // definition of constants
+        final int FLOUR = 4000;
+        final int BANANA = 6;
+        final int SUGAR = 2000;
+        final int BUTTER = 500;
+        final int COCOA = 500;
+
+        // definition of the variables
+
+    }
+}
