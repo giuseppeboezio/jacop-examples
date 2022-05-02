00dd1ca2accde300d3c7e5cef7fb2075dbc43747
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-05-02 18:25:01 +0200

    feat(cakes): added variables and constraints

--- cakes/src/main/java/app/Main.java
+++ cakes/src/main/java/app/Main.java
@@ -1,5 +1,9 @@
 package app;
 
+import org.jacop.constraints.LinearInt;
+import org.jacop.core.IntVar;
+import org.jacop.core.Store;
+
 /*
 cake baking problem
 Example 2.1.2 of Minizinc https://www.minizinc.org/doc-2.5.5/en/modelling.html
@@ -15,7 +19,25 @@ public class Main {
         final int BUTTER = 500;
         final int COCOA = 500;
 
+        final int MAX_NUM_CAKES = 100;
+
         // definition of the variables
+        Store store = new Store();
+
+        IntVar banana = new IntVar(store, "banana", 1, MAX_NUM_CAKES);
+        IntVar chocolate = new IntVar(store, "chocolate", 1, MAX_NUM_CAKES);
+        IntVar[] cakes = {banana, chocolate};
+
+        // definition of constraints
+        store.impose(new LinearInt(cakes, new int[] {250, 200},"<=", FLOUR));
+        store.impose(new LinearInt(new IntVar[] {banana}, new int[]{2}, "<=", BANANA));
+        store.impose(new LinearInt(cakes, new int[] {75, 150}, "<=", SUGAR));
+        store.impose(new LinearInt(cakes, new int[] {100, 150}, "<=", BUTTER));
+        store.impose(new LinearInt(new IntVar[]{chocolate}, new int[] {75}, "<=", COCOA));
+
+        
+
+
 
     }
 }
