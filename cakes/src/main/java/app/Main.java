765bc7b5ac051f0a43db1264e81fbbe6bf686048
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-05-02 18:58:48 +0200

    added cost function and related constraint

--- cakes/src/main/java/app/Main.java
+++ cakes/src/main/java/app/Main.java
@@ -27,6 +27,8 @@ public class Main {
         IntVar banana = new IntVar(store, "banana", 1, MAX_NUM_CAKES);
         IntVar chocolate = new IntVar(store, "chocolate", 1, MAX_NUM_CAKES);
         IntVar[] cakes = {banana, chocolate};
+        IntVar cost = new IntVar(store);
+
 
         // definition of constraints
         store.impose(new LinearInt(cakes, new int[] {250, 200},"<=", FLOUR));
@@ -35,7 +37,9 @@ public class Main {
         store.impose(new LinearInt(cakes, new int[] {100, 150}, "<=", BUTTER));
         store.impose(new LinearInt(new IntVar[]{chocolate}, new int[] {75}, "<=", COCOA));
 
-        
+        store.impose(new LinearInt(cakes, new int[] {-400, -450}, "==", cost));
+
+
 
 
 
